6a39523993ac4290cfe0710917c918d2eb3c298c
janvesely <vesjan007@gmail.com>
2026-04-02 11:13:06 +0200

    Added initial project files

--- src/com/tacticalarena/game/main/Main.java
+++ src/com/tacticalarena/game/main/Main.java
@@ -5,5 +5,6 @@ import com.tacticalarena.game.window.MenuFrame;
 public class Main {
     public static void main(String[] args) {
         MenuFrame menuFrame = new MenuFrame();
+        menuFrame.configureFrame();
     }
 }
