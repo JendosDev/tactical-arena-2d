7c4565713dc3e6489474c055d5d99c251b0eb16d
janvesely <vesjan007@gmail.com>
2026-04-11 10:57:53 +0200

    Implemented bullet class with coordinates, degrees, speed - Added method draw(Graphics g): drawing all bullets - Added method update(): how bullets go

--- src/com/tacticalarena/game/model/Bullet.java
+++ src/com/tacticalarena/game/model/Bullet.java
@@ -1,4 +1,76 @@
 package com.tacticalarena.game.model;
 
+import java.awt.*;
+
 public class Bullet {
+    private int x;
+    private int y;
+    private int dx, dy;
+    private int speed;
+
+    public Bullet(int x, int y, int dx, int dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+
+        this.speed = 10;
+    }
+
+    public Bullet() {
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 15, 25);
+    }
+
+    public void update() {
+        x += dx * speed;
+        y += dy * speed;
+    }
+
+    // region get, set
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    // endregion
 }
