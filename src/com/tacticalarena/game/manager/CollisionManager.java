7dbfddfdfc6d1d2556ddcc058221a84cf328ec06
janvesely <vesjan007@gmail.com>
2026-05-31 22:25:41 +0200

    Added CollisionManager - isColliding(Player p, Enemy e): controls how near is enemy to player

--- src/com/tacticalarena/game/manager/CollisionManager.java
+++ src/com/tacticalarena/game/manager/CollisionManager.java
@@ -1,4 +1,14 @@
 package com.tacticalarena.game.manager;
 
+import com.tacticalarena.game.interfaces.Collidable;
+import com.tacticalarena.game.model.Enemy;
+import com.tacticalarena.game.model.Player;
+
 public class CollisionManager {
+    public static boolean isColliding(Player player, Enemy enemy) {
+        return player.getX() < enemy.getX() + enemy.getWidth() &&
+                player.getX() + player.getWidth() > enemy.getX() &&
+                player.getY() < enemy.getY() + enemy.getHeight() &&
+                player.getY() + player.getHeight() > enemy.getY();
+    }
 }
