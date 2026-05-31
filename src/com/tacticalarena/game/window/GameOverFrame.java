f1d73952000cdd48bdf0dc42419ec82024962801
janvesely <vesjan007@gmail.com>
2026-05-31 22:30:02 +0200

    Added GameOverFrame to paint a window with game over title

--- src/com/tacticalarena/game/window/GameOverFrame.java
+++ src/com/tacticalarena/game/window/GameOverFrame.java
@@ -1,4 +1,21 @@
 package com.tacticalarena.game.window;
 
-public class GameOverFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameOverFrame extends JFrame {
+
+    public GameOverFrame() {
+        setTitle("Game Over");
+
+        JLabel label = new JLabel("GAME OVER", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 40));
+
+        add(label);
+
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
